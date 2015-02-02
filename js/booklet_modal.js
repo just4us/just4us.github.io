16a847ede53d71056546fae949d04cbeba7f4110
Sachin Khandelwal <sachin.khandelwal@paletly.com>
2015-02-02 20:43:43 +0530

    Page scroll to Deals sections on typing search query.

--- js/booklet_modal.js
+++ js/booklet_modal.js
@@ -1,10 +1,11 @@
 $(function() {
-   
+    // To close the modal on ESC key press
     $(document).keyup(function(ev){
         if(ev.keyCode == 27)
             $(".close-modal").trigger("click");
     });
     
+    //To scroll at Contact Us page on 'Get Booklet' button click 
     $(document).on("click", '.getbooklet', function(event) {
         var button = $(this);
         var mModalId = $(this).closest('.portfolio-modal').attr('id');
@@ -22,6 +23,13 @@ $(function() {
         });
         
         $('#' + mModalId).modal('hide');
+    });
+    
+    //Scroll to Deals section when use changes the search text.
+    $( '.searchbox input' ).keypress(function() {
+        $('html, body').stop().animate({
+            scrollTop: $('#portfolio').offset().top
+        }, 1500, 'easeInOutExpo');
     });
     
 });
