b5c4d832d25c6c42cd8560954879ee5f66a7663f
Sachin Khandelwal <sachin.khandelwal@paletly.com>
2015-04-01 16:12:00 +0530

    Changed error msg.

--- js/contact_me.js
+++ js/contact_me.js
@@ -47,7 +47,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems there is some error. Kindly contact admin@just4us.in.!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
